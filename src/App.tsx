1b672489c1df866aaebb4ee566411f11926f5d56
Helio Barolo <barolohelio@gmail.com>
2022-08-25 07:45:30 -0300

    Subindo repositório

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import { Button } from "./components/Button";
+
+export function App() {
+  return (
+    <div >
+      <h1>Hello World!!</h1>
+      <Button variant="primary"/>
+      <Button variant="secondary"/>
+      <Button variant="danger"/>
+      <Button variant="success"/>
+    </div>
+  )
+}
+
