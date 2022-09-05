75c6497e38c0160ecd49c1aaa026b29b17736ca6
Helio Barolo <barolohelio@gmail.com>
2022-09-05 07:57:25 -0300

    Retirando cache local

--- src/context/CyclesContext.tsx
+++ src/context/CyclesContext.tsx
@@ -33,15 +33,16 @@ export function CyclesContextProvider({children}:CyclesContextProviderProps) {
      {
     cycles: [],
     activeCycleId: null
-  },() => {
-    const storedStateAsJson = localStorage.getItem(
-      '@ignite-timer:cycles-state=1.0.0',
-    )
+  }
+  // ,() => {
+  //   const storedStateAsJson = localStorage.getItem(
+  //     '@ignite-timer:cycles-state=1.0.0',
+  //   )
 
-    if(storedStateAsJson){
-      return JSON.parse(storedStateAsJson)
-    }
-  },
+  //   if(storedStateAsJson){
+  //     return JSON.parse(storedStateAsJson)
+  //   }
+  // },
   )
 
   
